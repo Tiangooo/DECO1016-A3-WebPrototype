e489eaa8817a8ab69a36e148b55afa249b42735d
qintian0703 <qintian0703@gmail.com>
2020-11-19 14:05:09 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,59 @@
+
+// -------stickynav start--------//
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction1()};
+
+// Get the navbar
+var myTopnav = document.getElementById("myTopnav");
+
+// Get the offset position of the navbar
+var sticky = myTopnav.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction1() {
+  if (window.pageYOffset >= sticky) {
+    myTopnav.classList.add("sticky")
+  } else {
+    myTopnav.classList.remove("sticky");
+  }
+}
+// -------stickynav end--------//
+
+
+// -------scroll start-------//
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+// -------scroll end-------//
